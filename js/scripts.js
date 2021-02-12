03e473f9594b6b67cdddcdf014d8f4efca895e73
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-02-11 20:02:35 -0800

    possibly have function to iterate through the user inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,19 @@ const neighbor = "won't you be my neighbor?"
 
 const mrRoboger = function(input) {
   let numbers = [];
-}
+  for (let x = 0; x <=input; x++)
+    numbers.push(x.toString());
+};
+
+
 
 // user interface logic
 
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
-    userNumber.addEventListener('submit', ()=>{
+    //userNumber.addEventListener('submit', ()=>{
       console.log(input)
-    })
+    //})
   })
 })
