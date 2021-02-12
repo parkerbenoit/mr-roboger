ac9945a2a0ea1705d2fb6a93d33f37564f9d3770
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-02-11 19:48:54 -0800

    having trouble getting user input to console log

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,17 @@ const beep = "beep!"
 const boop = "boop!"
 const neighbor = "won't you be my neighbor?"
 
+const mrRoboger = function(input) {
+  let numbers = [];
+}
 
 // user interface logic
 
 $(document).ready(function() {
-
+  $("form#userNumber").submit(function(event) {
+    event.preventDefault();
+    userNumber.addEventListener('submit', ()=>{
+      console.log(input)
+    })
+  })
 })
