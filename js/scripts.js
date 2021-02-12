4c7bc20dede1d7be998cd51eb3ffe714c3481d2a
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-02-11 22:00:14 -0800

    creating a new js file with a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,41 @@ const neighbor = "won't you be my neighbor?"
 
 const mrRoboger = function(input) {
   let numbers = [];
-  for (let x = 0; x <=input; x++)
+  for (let x = 0; x <= input; x++) {
     numbers.push(x.toString());
+  };
+
+  let robogers = numbers.map(function(number) {
+    if (userNumber.includes("3")) {
+      //return userNumber = neighbor;
+      return neighbor;
+    } else {
+      return userNumber;
+    };
+  });
+  return robogers;
 };
 
 
 
+//console.log("testing 123")
+
+//console.log(numbers)
+
 // user interface logic
 
+// $(document).ready(function() {
+//   $(document).select()
+//   console.log("BEEP BEEP IM A JEEP");
+//   $("form#userNumber").submit(function(event) {
+//     event.preventDefault();
+    
 $(document).ready(function() {
-  $("form#userNumber").submit(function(event) {
+  $(document).on("submit", function(event) {
     event.preventDefault();
-    //userNumber.addEventListener('submit', ()=>{
-      console.log(input)
-    //})
-  })
-})
+
+    let x = $("input").val();
+
+    console.log(x);
+  });
+});
